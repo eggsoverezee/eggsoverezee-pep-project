dece1d1e4b9ca4aaceca0431ace465c429832e5c
eggsoverezee <35790160+eggsoverezee@users.noreply.github.com>
2023-09-20 15:40:59 -0400

    Update SocialMediaController.java

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -16,11 +16,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 
-/**
- * TODO: You will need to write your own endpoints and handlers for your controller. The endpoints you will need can be
- * found in readme.md as well as the test cases. You should
- * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
- */
+
 public class SocialMediaController {
     AccountService accountService;
     MessageService messageService;
@@ -29,11 +25,7 @@ public class SocialMediaController {
         accountService = new AccountService();
         messageService = new MessageService();
     }
-    /**
-     * In order for the test cases to work, you will need to write the endpoints in the startAPI() method, as the test
-     * suite must receive a Javalin object from this method.
-     * @return a Javalin app object which defines the behavior of the Javalin controller.
-     */
+
     public Javalin startAPI() {
         Javalin app = Javalin.create();
         app.get("/messages", this::getAllMessagesHandler);
